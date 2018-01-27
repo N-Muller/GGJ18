3939c4a5f7dbacf656eb2e5bd7bc078f3021b411
n-muller <mullernicolas91@gmail.com>
2018-01-27 06:55:32 +0100

    Working network POC

---
+++ Assets/Scripts/Network/Player.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine.Networking;
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+class Player : NetworkBehaviour
+{
+	enum Type { None, Peasant, Fourbe }
+
+	[Serializable]
+	public class Card
+	{
+		int id;
+
+		public Card(int id)
+		{
+			this.id = id;
+		}
+	}
+
+	[SyncVar]
+	Type type = Type.None;
+	[SyncVar]
+	List<Card> hand;
+
+
+
+
+
+
+	[ClientRpc]
+	void RpcDamage(int amount)
+	{
+		Debug.Log("Took damage:" + amount);
+	}
+
+
+	[Command]
+	void CmdDamage(int amount)
+	{
+		print ("eeee");
+		TakeDamage (amount);
+	}
+
+
+	public void DoCmdDamage(int amount)
+	{
+		if (!isLocalPlayer)
+			return;
+		
+		CmdDamage (amount);
+	}
+
+	public void TakeDamage(int amount)
+	{
+		if (!isServer)
+			return;
+
+		// health -= amount;
+		RpcDamage(amount);
+	}
+}
---
+++ Assets/Scripts/Utility/KeyboardEventBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class KeyboardEventBehaviour : MonoBehaviour {
+
+	public KeyCode key;
+	public UnityEvent OnToggleTrue;
+	public UnityEvent OnToggleFalse;
+	public UnityEvent OnDown;
+	public UnityEvent OnUp;
+
+	[Tooltip("Initial state.\nWill change on first key press.")]
+	bool toggle = false;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (key)) {
+			toggle = !toggle;
+			OnUp.Invoke ();
+			if (toggle)
+				OnToggleTrue.Invoke ();
+			else
+				OnToggleFalse.Invoke ();
+		}
+
+		if (Input.GetKeyUp (key)) 
+			OnDown.Invoke ();
+	}
+}
