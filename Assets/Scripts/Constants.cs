01a9ceb7abd5d7e388ef11a245399b58e622858d
n-muller <mullernicolas91@gmail.com>
2018-01-27 18:40:16 +0100

    Constants && Player intializatino

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,7 @@
+﻿
+
+public static class Constants {
+
+	public static int HandSize = 2;
+	public static int DeckSize = 20;
+}
--- Assets/Scripts/Network/Player.cs
+++ Assets/Scripts/Network/Player.cs
@@ -8,14 +8,28 @@ class Player : NetworkBehaviour
 	[Serializable]
 	public enum Role { Undefined, Fourbe, Gentil }
 
+	[Serializable]
+	public class SerializedPlayerData
+	{
+		public Role role;
+		public List<int> hand;
+
+		public SerializedPlayerData( Player p)
+		{
+			role = p.role;
+			hand = p.hand;
+		}
+	}
+
+
 	public static Player LocalPlayer;
 
-	public static List<Player> Players;
-
+	public static List<Player> Players = new List<Player> ();
 
 	public Role role = Role.Undefined;
-
 	public List<int> hand;
+
+	public static bool PlayersInitialized = false;
 
 	public bool ready = false;
 
@@ -24,7 +38,7 @@ class Player : NetworkBehaviour
 		if (isLocalPlayer)
 			LocalPlayer = this;
 
-		Players = new List<Player> ();
+		Players.Add (this);
 	}
 
 
@@ -33,7 +47,9 @@ class Player : NetworkBehaviour
 	{
 		ready = true;
 
-		if (!Players.Exists (p => p.ready = false)) {
+		Debug.Assert (isServer == true, "This function is supposed to be called by the server");
+
+		if (!Players.Exists (p => p.ready = false) && !PlayersInitialized) {
 			InitPlayers ();
 		}
 	}
@@ -50,20 +66,44 @@ class Player : NetworkBehaviour
 
 	static void InitPlayers()
 	{
+
 		int fourbe = UnityEngine.Random.Range (0, Players.Count);
 		List<int> deck = new List<int> ();
 
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < Constants.DeckSize; i++) 
+		{
 			deck.Add (i);
 		}
 
 
 		for (int i = 0; i < Players.Count; i++) {
-			Players [i].role = i == fourbe ? Role.Fourbe : Role.Gentil;
+			Player p = Players [i];
+
+			p.role = i == fourbe ? Role.Fourbe : Role.Gentil;
+
+			for (int j = 0; j < Constants.HandSize; j++) {
+				int cardId = UnityEngine.Random.Range (0, deck.Count);
+				p.hand.Add (deck [cardId]);
+				deck.RemoveAt (cardId);
+			}
+
+			SerializedPlayerData data = new SerializedPlayerData (p);
+
+			p.RpcSyncClient (JsonUtility.ToJson (data));
 		}
 
+		PlayersInitialized = true;
 	}
+		
+	[ClientRpc]
+	void RpcSyncClient(string serializedPlayerData)
+	{
+		
+		SerializedPlayerData data = JsonUtility.FromJson<SerializedPlayerData> (serializedPlayerData);
 
+		role = data.role;
+		hand = data.hand;
+	}
 
 	[SyncVar]
 	int health;
