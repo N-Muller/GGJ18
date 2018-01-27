7998ac74a13357283befc05cc3c67463b92a5a98
Hippunk <arthur.ramolet@hotmail.fr>
2018-01-27 02:24:56 +0100

    template carte, script flou, sprites pièces

---
+++ Assets/Scripts/Zone.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Zone : MonoBehaviour, IPointerClickHandler {
+
+	public bool flou = true;
+	public Sprite imagePasfloue;
+	public Sprite imageFloue;
+
+	void OnEnable(){
+		GetComponent<SpriteRenderer> ().sprite = flou?imageFloue:imagePasfloue;
+	}
+
+	#region IPointerClickHandler implementation
+	public void OnPointerClick (PointerEventData eventData)
+	{
+		flou = false;
+	}
+	#endregion
+}
